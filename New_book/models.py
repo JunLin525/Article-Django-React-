8da28da47b733c15705593df0686083ee3fe91c0
junli525 <junlin5525@gmail.com>
2023-06-26 17:08:31 +0000

    feat: 🎸 add New_book comment
    add new book comment model and just want to make it as an api

--- New_book/models.py
+++ New_book/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 import uuid
+from django.conf import settings
 # Create your models here.
 
 
@@ -18,6 +19,7 @@ class New_Book(models.Model):
             MinValueValidator(0000000000000)
         ]
     )
+    Cover = models.ImageField(upload_to="covers/", blank=True, null=True)
     Abstract = models.TextField()
 
     def __str__(self):
@@ -26,3 +28,14 @@ class New_Book(models.Model):
     class Meta:
         verbose_name_plural = 'New_Books'
         verbose_name = 'New_book'
+
+
+class Comments(models.Model):
+    article = models.ForeignKey(New_Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return f'{self.article} - {self.title}'
