95c391c3d36797fd9c1ad33fc98c08b8d583eaa0
junli525 <junlin5525@gmail.com>
2023-06-29 15:48:01 +0000

    fix: 🐛 fix unuse apps
    just delete test_app to maintain the clearance of this application.

--- test_app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import article
-
-
-class ArticleSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = article
-        fields = ("title", "body")
--- test_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import Article_APIView
-from .views import ArticleView
-urlpatterns = [
-    path('', ArticleView.as_view(), name='article_list'),
-    path('/<int:pk>', Article_APIView.as_view(), name='article_detail'),
-]
